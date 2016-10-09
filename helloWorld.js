c3cd82a5c46b950ca982e28116004024aa0a3789
cvanlent <collemc@umich.edu>
2016-10-09 12:04:23 -0400

    Added line about Sublime

--- helloWorld.js
+++ helloWorld.js
@@ -1,3 +1,5 @@
 var name = "Jimmy";
 console.log("Hello " + name);
-console.log("Now I actually on GitHub")
+console.log("Now I actually on GitHub");
+console.log("I am making this change from Sublime");
+
